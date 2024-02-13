2d2b586ab3a9282d46c804c38246b0ff38f602e3
SATHYASEELAN-S <sathyaseelan0@yahoo.com>
2024-02-13 19:13:22 +0530

    PUSH

--- js/index.js
+++ js/index.js
@@ -1,70 +1,83 @@
-function sendMail(){
-    var params={
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        message:document.getElementById("message").value,
-    };
+function sendMail() {
 
-    const serviceID="service_wrxxp1o";
-    const templateID="template_a5kcq0s";
-    
-    emailjs 
-    .send (serviceID,templateID,params)
-    .then((res)=>
-    {
-        document.getElementById("name").value="";
-        document.getElementById("email").value="";
-        document.getElementById("message").value="";
+
+  var name = document.getElementById("name").value ;
+  var email = document.getElementById("email").value ;
+  var message = document.getElementById("message").value ;
+
+
+
+  if (name !== "" && email !== "" && message !== "") {
+    var params = {
+      name, email, message,
+    };
+    const serviceID = "service_wrxxp1o";
+    const templateID = "template_a5kcq0s";
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
         console.log(res);
-        
-        alert("your message sent successfully ");
-    })
-.catch((err)=>console.log(err));
-    
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "your message sent successfully ",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      })
+      .catch((err) => console.log(err));
+
+  } else {
+    Swal.fire("Every field is important! ");
+  }
+
+
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    var navbar = document.querySelector('.navbar');
+  var navbar = document.querySelector('.navbar');
 
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > 0) {
-        navbar.classList.add('scrolled');
-      } else {
-        navbar.classList.remove('scrolled');
-      }
-    });
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
   });
+});
 
-  // -----------------------------------------------
+// -----------------------------------------------
 
 
-  const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
+const scrollActive = () => {
+  const scrollDown = window.scrollY
 
-  sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }                                                    
-    })
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
 }
 window.addEventListener('scroll', scrollActive)
